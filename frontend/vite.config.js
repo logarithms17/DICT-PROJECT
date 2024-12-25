e87231bb3b89d38b9bbfc9159d27e1da34ed4112
Neil Bryan Apostol <neilbryanapostol@gmail.com>
2024-12-25 14:16:30 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend/', // Change this if your frontend is deployed to a different subdirectory or leave it as '/' if it's at the root
   plugins: [react()],
 })
